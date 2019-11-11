af0807f7ae6366984f92dea1a9c07b6bd238bdb2
Konsta Purtsi <konsta.purtsi@vincit.fi>
2019-11-11 19:38:44 +0200

    deploy

---
+++ precache-manifest.8a10c63015bf11fc42537b82d6f17c18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61017f1bded5434682b75790459ffd99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bfb511dc36c96d2a12d",
+    "url": "/static/css/main.06e03e7b.chunk.css"
+  },
+  {
+    "revision": "212d1684633e61ff2d85",
+    "url": "/static/js/2.113ec891.chunk.js"
+  },
+  {
+    "revision": "1bfb511dc36c96d2a12d",
+    "url": "/static/js/main.48602f75.chunk.js"
+  },
+  {
+    "revision": "14184347313b63149b93",
+    "url": "/static/js/runtime-main.bd7e3a63.js"
+  }
+]);
